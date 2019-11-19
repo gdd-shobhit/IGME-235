b10c50794205cb6ff411324b53306e07d8936de1
Shobhit Dhamania <47251601+gdd-shobhit@users.noreply.github.com>
2019-11-19 14:31:40 -0500

    files

---
+++ hello.php
@@ -0,0 +1,22 @@
+<!DOCTYPE>
+<html lang="en">
+<header>
+	<meta charset="UTF=8">
+	<title>
+	OK
+	</title>
+</header
+
+
+
+
+
+<body>
+
+<?php
+	echo "hello World";
+	echo "<p>It's " . date('r');
+	?>
+	
+</body>
+</html>
